d60ec1f62911b41b47e6c2989cf357634b399199
sergey-emelyanov <emelia35@yandex.ru>
2023-07-27 06:34:24 +0300

    add pytho-dotenv and hide secret-key

--- hexlet_django_blog/settings.py
+++ hexlet_django_blog/settings.py
@@ -10,7 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-xy91uea_c$36!))l5zr4ma&(hv676h+&3^=m4^irg-b3w$$nf#'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
