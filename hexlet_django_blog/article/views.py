a1739c1f0614201302b8733693194727a3356642
sergey-emelyanov <emelia35@yandex.ru>
2023-07-27 05:42:23 +0300

    add Article model in views

--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -1,22 +1,22 @@
 from django.shortcuts import render, redirect
 from django.views import View
 from django.urls import reverse
+from hexlet_django_blog.article.models import Article
 
 
 # Create your views here.
 
 class IndexView(View):
 
-    def get(self, request, tags, article_id):
+    def get(self, request):
+        articles = Article.objects.all()
         return render(request, template_name='articles/index.html', context={
-            'tags': tags,
-            'article_id': article_id
+            'articles': articles
 
         })
 
-
-def root(request):
-    return redirect(reverse('article', kwargs={
-        'tags': 'python',
-        'article_id': 42
-    }))
+# def root(request):
+#     return redirect(reverse('article', kwargs={
+#         'tags': 'python',
+#         'article_id': 42
+#     }))
