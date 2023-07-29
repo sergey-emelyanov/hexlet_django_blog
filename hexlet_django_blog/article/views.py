81a8ee31b150b2e81d341842e49036a1d8c443fb
sergey-emelyanov <emelia35@yandex.ru>
2023-07-29 16:46:15 +0300

    add a CRUD list, changes index templates

--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -8,15 +8,9 @@ from hexlet_django_blog.article.models import Article
 
 class IndexView(View):
 
-    def get(self, request):
+    def get(self, request, *args, **kwargs):
         articles = Article.objects.all()
         return render(request, template_name='articles/index.html', context={
             'articles': articles
 
         })
-
-# def root(request):
-#     return redirect(reverse('article', kwargs={
-#         'tags': 'python',
-#         'article_id': 42
-#     }))
