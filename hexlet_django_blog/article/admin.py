b6c6c9b65dd67f396eea9d52f61e236358cb9945
sergey-emelyanov <emelia35@yandex.ru>
2023-07-29 12:02:13 +0300

    add and register admin model

--- hexlet_django_blog/article/admin.py
+++ hexlet_django_blog/article/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from django.contrib.admin import DateFieldListFilter
+from .models import Article
+
 
 # Register your models here.
+
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    search_fields = ['name', 'body']
+    list_display = ('name', 'timestamp')
+    list_filter = (('timestamp',DateFieldListFilter),)
+
+
+
